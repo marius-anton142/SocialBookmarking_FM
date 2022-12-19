d23bc40ebdbc5d4ac3e9eae262042603af98a525
dinuf <dinuf@WMACHINE>
2022-12-19 14:01:53 +0200

    Add project files.

---
+++ SocialBookmarking_FM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialBookmarking_FM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
