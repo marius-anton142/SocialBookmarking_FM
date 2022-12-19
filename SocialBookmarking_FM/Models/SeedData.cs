8facdbfab70f13ca2f1b9af77e61fcf9b642fd9c
fredtux <fredtux@noreply.github.com>
2022-12-19 16:39:24 +0200

    Dropped moderator

--- SocialBookmarking_FM/Models/SeedData.cs
+++ SocialBookmarking_FM/Models/SeedData.cs
@@ -20,7 +20,6 @@ namespace SocialBookmarking_FM.Models
                 
                 context.Roles.AddRange(
                 new IdentityRole { Id = "2c5e174e-3b0e-446f-86af483d56fd7210", Name = "Admin", NormalizedName = "Admin".ToUpper() },
-                new IdentityRole { Id = "2c5e174e-3b0e-446f-86af483d56fd7211", Name = "Moderator", NormalizedName = "Editor".ToUpper() },
                 new IdentityRole { Id = "2c5e174e-3b0e-446f-86af483d56fd7212", Name = "User", NormalizedName = "User".ToUpper() }
                 );
                 
@@ -36,17 +35,6 @@ namespace SocialBookmarking_FM.Models
                     Email = "admin@test.com",
                     NormalizedUserName = "ADMIN@TEST.COM",
                     PasswordHash = hasher.HashPassword(null,"Admin1!")
-                },
-               new ApplicationUser
-                {
-
-                 Id = "8e445865-a24d-4543-a6c6-9443d048cdb1",
-                 UserName = "editor@test.com",
-                EmailConfirmed = true,
-                NormalizedEmail = "MODERATOR@TEST.COM",
-                Email = "editor@test.com",
-                NormalizedUserName = "MODERATOR@TEST.COM",
-                PasswordHash = hasher.HashPassword(null,"Editor1!")
                 },
                new ApplicationUser
                {
@@ -66,11 +54,6 @@ namespace SocialBookmarking_FM.Models
                     RoleId = "2c5e174e-3b0e-446f-86af483d56fd7210",
                     UserId = "8e445865-a24d-4543-a6c6-9443d048cdb0"
                 },
-               new IdentityUserRole<string>
-               {
-                   RoleId = "2c5e174e-3b0e-446f-86af483d56fd7211",
-                   UserId = "8e445865-a24d-4543-a6c6-9443d048cdb1"
-               },
                new IdentityUserRole<string>
                {
                    RoleId = "2c5e174e-3b0e-446f-86af483d56fd7212",
