2647b6be8afc4b4ca3c3c55b8b060518a5d672df
fredtux <fredtux@noreply.github.com>
2022-12-20 04:49:02 +0200

    Added pagination

--- SocialBookmarking_FM/Controllers/HomeController.cs
+++ SocialBookmarking_FM/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace SocialBookmarking_FM.Controllers
 {
     public class HomeController : Controller
     {
+        private const int PAGINATION = 2;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
@@ -18,15 +19,72 @@ namespace SocialBookmarking_FM.Controllers
         private readonly ApplicationDbContext db;
         
 
-        public IActionResult Index()
+        public IActionResult Index(string? order, int? start)
         {
-            var bkm = (from x in db.Bookmarks 
-                       join y in db.Users
-                       on x.UserId equals y.Id
-                       
-                       select new {b = x, u = y}).OrderByDescending(x => x.b.Date).ToList();
-            ViewBag.bkm = bkm;
-            return View();
+            order = order ?? "date";
+            start = start ?? 0;
+
+            ViewBag.prevOrder = order;
+
+            if ((string)order == "date")
+            {
+                var qbkm = (from x in db.Bookmarks
+                            join y in db.Users
+                            on x.UserId equals y.Id
+
+                            select new { b = x, u = y }).OrderByDescending(x => x.b.Date);
+                if (qbkm.Count() - start - 1 > 0)
+                {
+                    ViewBag.next = start + PAGINATION ;
+                } else
+                {
+                    ViewBag.next = -1;
+                }
+
+                if (start > 0)
+                {
+                    ViewBag.prev = start - PAGINATION;
+                }
+                else
+                {
+                    ViewBag.prev = -1;
+                }
+
+                var bkm = qbkm.Skip((int)start).Take(PAGINATION).ToList();
+
+                ViewBag.bkm = bkm;
+                ViewBag.order = "rating";
+                return View();
+            } else
+            {
+                var qbkm = (from x in db.Bookmarks
+                           join y in db.Users
+                           on x.UserId equals y.Id
+
+                           select new { b = x, u = y }).OrderByDescending(x => x.b.rating).Skip((int)start).Take(2).ToList();
+
+                if (qbkm.Count() - start - 1 > 0)
+                {
+                    ViewBag.next = start + PAGINATION - 1;
+                } else
+                {
+                    ViewBag.next = -1;
+                }
+
+                if (start > 0)
+                {
+                    ViewBag.prev = start - PAGINATION + 1;
+                }
+                else
+                {
+                    ViewBag.prev = -1;
+                }
+
+                var bkm = qbkm.Skip((int)start).Take(PAGINATION).ToList();
+                ViewBag.bkm = bkm;
+                ViewBag.order = "date";
+                return View();
+            }
         }
 
         public IActionResult Privacy()
--- SocialBookmarking_FM/Program.cs
+++ SocialBookmarking_FM/Program.cs
@@ -110,7 +110,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{order?}/{start?}");
 
 app.MapRazorPages();
 
